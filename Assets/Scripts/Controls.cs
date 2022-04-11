29a091711fdb56e2333f879deed4a22817751c87
Kiera Acitelli <kacitelli@gmail.com>
2022-04-11 16:19:06 -0400

    Add sprites & L/R movement

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float movespeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+        }
+    }
+}
