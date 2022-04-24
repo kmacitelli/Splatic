be520ea012e807699a9c636dc9b6e957e0b9bfba
Kiera Acitelli <kacitelli@gmail.com>
2022-04-24 15:17:36 -0400

    Add jumping controls

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -7,23 +7,45 @@ public class Controls : MonoBehaviour
 
     public Rigidbody2D rb;
     public float movespeed = 1.0f;
+    public float jumpSpeed = 3.0f;
+    public float prevYVelocity;
+    public bool jumping = false;
+    float lockPos = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        transform.rotation = Quaternion.Euler(lockPos, lockPos, lockPos);
     }
 
-    // Update is called once per frame
+
+
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             rb.velocity = new Vector2(-movespeed, rb.velocity.y);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(movespeed, rb.velocity.y);
         }
+        if (Input.GetKey(KeyCode.Space) && (jumping == false))
+        {
+            jumping = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+
+        
+
+
+        //Prevent double jumps. This checks "on the ground"
+        if ((rb.velocity.y == 0) && ((rb.velocity.y - prevYVelocity) == 0))
+        {
+            jumping = false;
+        }
+
+        prevYVelocity = rb.velocity.y;
     }
 }
